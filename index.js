38d794c13d433faf79f226805eeabf8b0319122e
issafoucault <alejandro.foucault@issprops.com>
2023-01-03 22:39:42 -0800

    Section 40: Middleware: The Key To Express.

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-
+const port = process.env.PORT || 3001;
 
 app.use(morgan('tiny'));
 
 app.use((req, res, next) => {
-    req.requestTime = Date.now();
-    console.log(req.method, req.path);
-    next();
-})
+  req.requestTime = Date.now();
+  console.log(req.method, req.path);
+  next();
+});
 
 app.use('/dogs', (req, res, next) => {
-    console.log("I LOVE DOGS!!")
-    next();
-})
+  console.log('I LOVE DOGS!!');
+  next();
+});
 
 const verifyPassword = (req, res, next) => {
-    const { password } = req.query;
-    if (password === 'chickennugget') {
-        next();
-    }
-    res.send("YOU NEED A PASSWORD!")
-}
-
+  const { password } = req.query;
+  if (password === 'chickennugget') {
+    next();
+  }
+  res.send('YOU NEED A PASSWORD!');
+};
 
 // app.use((req, res, next) => {
 //     console.log("THIS IS MY FIRST MIDDLEWARE!!!")
@@ -39,26 +38,24 @@ const verifyPassword = (req, res, next) => {
 //     return next();
 // })
 
-
 app.get('/', (req, res) => {
-    console.log(`REQUEST DATE: ${req.requestTime}`)
-    res.send('HOME PAGE!')
-})
+  console.log(`REQUEST DATE: ${req.requestTime}`);
+  res.send('HOME PAGE!');
+});
 
 app.get('/dogs', (req, res) => {
-    console.log(`REQUEST DATE: ${req.requestTime}`)
-    res.send('WOOF WOOF!')
-})
+  console.log(`REQUEST DATE: ${req.requestTime}`);
+  res.send('WOOF WOOF!');
+});
 
 app.get('/secret', verifyPassword, (req, res) => {
-    res.send('MY SECRET IS: Sometimes I wear headphones in public so I dont have to talk to anyone')
-})
+  res.send(
+    'MY SECRET IS: Sometimes I wear headphones in public so I dont have to talk to anyone'
+  );
+});
 
 app.use((req, res) => {
-    res.status(404).send('NOT FOUND!')
-})
+  res.status(404).send('NOT FOUND!');
+});
 
-
-app.listen(3000, () => {
-    console.log('App is running on localhost:3000')
-})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
